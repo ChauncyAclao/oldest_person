950a5992d84bba1edd3b2e244a5730f8fa323a5b
ChauncyAclao <rheignchauncy@gmail.com>
2024-10-21 15:37:43 +0800

    input another entry

--- oldest_person.py
+++ oldest_person.py
@@ -4,24 +4,31 @@
 #input another entry?
 #find oldest person
 
-#input name age
-
-#validate name
+#iput another entry
 while True:
+    #input name age
+    #validate name
     try:
         while True:
             name = input("Name: ")
 
             if len(name) >1:
-             break
-
+                    break
 #validate age
         while True:
             age = int(input("age "))
 
             if age > 0 :
-                break
+                    break
+
+#for another entry
     except:
-        print("try again")
+        print("Invalid Retry")
+
+    new_entry = input("Give another entry? (yes or no) ")
+    if new_entry == "no":
+            break
 
 
+        
+        
